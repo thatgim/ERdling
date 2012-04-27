1d4e26f4a126cd97c957dcb463eb9332c25234b7
Felix Schmeing <felix@vitreous>
2012-04-27 20:31:22 +0200

    now the real project

---
+++ projects/ERdling/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: felix
+ *
+ * Created on 27. April 2012, 20:30
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
