d0ce827f1195abf0c735c0cc020ddafd0d835995
Felix Schmeing <gimbar@breax.org>
2012-05-02 09:25:59 +0200

    added mainclass

---
+++ code/ERdling/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: felix
+ *
+ * Created on 2. Mai 2012, 09:25
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
