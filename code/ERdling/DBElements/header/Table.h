3ba99e850a5ecdd2edb08db3bf53e98da68d19e1
Felix Schmeing <gimbar@breax.org>
2012-05-04 21:56:30 +0200

    added table class

---
+++ code/ERdling/DBElements/header/Table.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Table.h
+ * Author: felix
+ *
+ * Created on 4. Mai 2012, 21:55
+ */
+
+#ifndef TABLE_H
+#define	TABLE_H
+
+class Table {
+public:
+    Table();
+    Table(const Table& orig);
+    virtual ~Table();
+private:
+
+};
+
+#endif	/* TABLE_H */
+
---
+++ code/ERdling/DBElements/src/Table.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Table.cpp
+ * Author: felix
+ * 
+ * Created on 4. Mai 2012, 21:55
+ */
+
+#include "Table.h"
+
+Table::Table() {
+}
+
+Table::Table(const Table& orig) {
+}
+
+Table::~Table() {
+}
+
